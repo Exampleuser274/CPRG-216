786222326e9f15c24e36f3ce0264e2bac6fb4b3d
Exampleuser274 <alexanderkovach6@gmail.com>
2025-09-18 15:43:25 -0600

    updated class1

---
+++ class1.py
@@ -0,0 +1,15 @@
+#Third class demo
+'''
+In this class we will work on variables, operators and other stuff
+'''
+
+#Review
+#assignment
+w = "four" #string
+x = 4 #integer
+y = 4.0 #float
+z = False #bool
+print("The variable w is",w,"and the type is",type(w))
+print("the variable x is",x,"and the type is",type(x))
+print("the variable y is",y,"and the type is",type(y))
+print("the variable z is",z,"and the type is",type(z))

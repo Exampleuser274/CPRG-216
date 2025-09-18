1144be2a50bcfa6a117f871389eb8bcda6046e20
Exampleuser274 <alexanderkovach6@gmail.com>
2025-09-18 16:05:31 -0600

    added functions

--- class1.py
+++ class1.py
@@ -13,3 +13,16 @@ print("The variable w is",w,"and the type is",type(w))
 print("the variable x is",x,"and the type is",type(x))
 print("the variable y is",y,"and the type is",type(y))
 print("the variable z is",z,"and the type is",type(z))
+
+#some functions call: print, input, int, float, str, bool
+
+strnum = "43"
+intnum = int(strnum) #convert to int
+num = 3
+num_f = float(num)
+num2 = 3.4
+num2_i = int(num2)
+num2str = str(num2)
+#printing the string and int
+print(intnum)
+print(strnum)

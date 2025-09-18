eb9182339aff8d7a28685b4db4886d3f7a0c4b0d
Exampleuser274 <alexanderkovach6@gmail.com>
2025-09-18 15:23:15 -0600

    added user input

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
-r = 5
-PI = 3.14
+user_input = input("What is the radius? ")
+r = int(user_input)
+PI = 22/7
 area = PI * r**2
 circ = 2*PI*r
 print("the circumference is", circ, 'and the area is', area)
